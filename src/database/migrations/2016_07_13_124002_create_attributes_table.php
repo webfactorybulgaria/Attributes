f7646dd6f1a30887128758734e51a9a78e22b8df
Nasko <nasko@webfactory.bg>
2017-02-08 09:59:06 +0200

    change migration order

--- src/database/migrations/2016_07_13_124002_create_attributes_table.php
+++ src/database/migrations/2016_07_13_124002_create_attributes_table.php
@@ -12,6 +12,27 @@ class CreateAttributesTable extends Migration
      */
     public function up()
     {
+
+        Schema::create('attribute_groups', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('type');
+            $table->timestamps();
+        });
+
+        Schema::create('attribute_group_translations', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('attribute_group_id')->unsigned();
+            $table->string('locale');
+            $table->boolean('status')->default(0);
+            $table->string('value');
+            $table->timestamps();
+            $table->unique(['attribute_group_id', 'locale']);
+            $table->foreign('attribute_group_id')->references('id')->on('attribute_groups')->onDelete('cascade');
+
+        });
+
         Schema::create('attributes', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
@@ -35,25 +56,6 @@ class CreateAttributesTable extends Migration
 
         });
 
-        Schema::create('attribute_groups', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->integer('type');
-            $table->timestamps();
-        });
-
-        Schema::create('attribute_group_translations', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->integer('attribute_group_id')->unsigned();
-            $table->string('locale');
-            $table->boolean('status')->default(0);
-            $table->string('value');
-            $table->timestamps();
-            $table->unique(['attribute_group_id', 'locale']);
-            $table->foreign('attribute_group_id')->references('id')->on('attribute_groups')->onDelete('cascade');
-
-        });
     }
 
     /**
